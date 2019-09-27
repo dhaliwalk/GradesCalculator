8015ddc3b0a34654256027191b2b1de2fec34479
dhaliwalk <kevindhal@gmail.com>
2019-09-27 15:55:42 -0700

    adding input checks

--- public/stylesheets/calculator.js
+++ public/stylesheets/calculator.js
@@ -24,8 +24,60 @@ function meanFunction() {
   var a32 = document.getElementById("A3Grade2").value;
   var a41 = document.getElementById("A4Grade1").value;
   var a42 = document.getElementById("A4Grade2").value;
-  var total = ((a11/a12)+(a21/a22)+(a31/a32)+(a41/a42))/4;
+
+  if (a11.length == 0) {
+    a11 = 0;
+  }
+  if (a12.length == 0) {
+    a12 = 0;
+  }
+  if (a21.length == 0) {
+    a21 = 0;
+  }
+  if (a22.length == 0) {
+    a22 = 0;
+  }
+  if (a31.length == 0) {
+    a131 = 0;
+  }
+  if (a32.length == 0) {
+    a32 = 0;
+  }
+  if (a41.length == 0) {
+    a41 = 0;
+  }
+  if (a42.length == 0) {
+    a42 = 0;
+  }
+
+  var a1perc = a11/a12;
+  var a2perc = a21/a22;
+  var a3perc = a31/a32;
+  var a4perc = a41/a42;
+  var count = 4;
+  if (a1perc > 101 || a12 == 0) {
+    a1perc = 0;
+    count--;
+  }
+  if (a2perc > 101 || a22 == 0) {
+    a2perc = 0;
+    count--;
+  }
+  if (a3perc > 101 || a32 == 0) {
+    a3perc = 0;
+    count--;
+  }
+  if (a4perc > 101 || a42 == 0) {
+    a4perc = 0;
+    count--;
+  }
+  if (count == 0) {
+    document.getElementById("res").innerHTML = "Invalid Inputs";
+  }
+  else {
+  var total = (a1perc*1+a2perc*1+a3perc*1+a4perc*1)/count;
   document.getElementById("res").innerHTML = (total*100).toFixed(2)+"/100";
+  }
 }
 function weightedFunction() {
   var a11 = document.getElementById("A1Grade1").value;
@@ -43,10 +95,73 @@ function weightedFunction() {
   var a41 = document.getElementById("A4Grade1").value;
   var a42 = document.getElementById("A4Grade2").value;
   var a4w = document.getElementById("A4Weight").value;
-  var divider  = a1w*1+a2w*1+a3w*1+a4w*1;
-  var total = (((a11/a12)*a1w)+((a21/a22)*a2w)+((a31/a32)*a3w)+((a41/a42)*a4w));
-  var result = (total/divider)*100;
-  document.getElementById("res").innerHTML = result.toFixed(2)+"/100";
+
+  if (a11.length == 0) {
+    a11 = 0;
+  }
+  if (a12.length == 0) {
+    a12 = 0;
+  }
+  if (a21.length == 0) {
+    a21 = 0;
+  }
+  if (a22.length == 0) {
+    a22 = 0;
+  }
+  if (a31.length == 0) {
+    a131 = 0;
+  }
+  if (a32.length == 0) {
+    a32 = 0;
+  }
+  if (a41.length == 0) {
+    a41 = 0;
+  }
+  if (a42.length == 0) {
+    a42 = 0;
+  }
+  if (a1w.length == 0) {
+    a1w = 0;
+  }
+  if (a2w.length == 0) {
+    a2w = 0;
+  }
+  if (a3w.length == 0) {
+    a3w = 0;
+  }
+  if (a4w.length == 0) {
+    a4w = 0;
+  }
+
+  var a1perc = a11/a12;
+  var a2perc = a21/a22;
+  var a3perc = a31/a32;
+  var a4perc = a41/a42;
+  if (a1perc > 101 || a12 == 0) {
+    a1perc = 0;
+    a1w = 0;
+  }
+  if (a2perc > 101 || a22 == 0) {
+    a2perc = 0;
+    a2w = 0;
+  }
+  if (a3perc > 101 || a32 == 0) {
+    a3perc = 0;
+    a3w = 0;
+  }
+  if (a4perc > 101 || a42 == 0) {
+    a4perc = 0;
+    a4w = 0;
+  }
+  var count = a1w*1+a2w*1+a3w*1+a4w*1;
+  if (count == 0) {
+    document.getElementById("res").innerHTML = "Invaild Inputs";
+  }
+  else {
+    var total = ((a1perc*a1w)+(a2perc*a2w)+(a3perc*a3w)+(a4perc*a4w));
+    var result = (total/count)*100;
+    document.getElementById("res").innerHTML = result.toFixed(2)+"/100";
+  }
 }
 
 function A1per() {
@@ -56,7 +171,7 @@ function A1per() {
   if (total < 101) {
     document.getElementById("A1Percent").innerHTML = (total).toFixed(2)+"%";
   } else {
-    document.getElementById("A1Percent").innerHTML = "Percentage Too High!";
+    document.getElementById("A1Percent").innerHTML = "Percentage invalid for other calculations!";
   }
 }
 function A2per() {
@@ -66,7 +181,7 @@ function A2per() {
   if (total < 101) {
     document.getElementById("A2Percent").innerHTML = (total).toFixed(2)+"%";
   } else {
-    document.getElementById("A2Percent").innerHTML = "Percentage Too High!";
+    document.getElementById("A2Percent").innerHTML = "Percentage invalid for other calculations!";
   }
 }
 function A3per() {
@@ -76,7 +191,7 @@ function A3per() {
   if (total < 101) {
     document.getElementById("A3Percent").innerHTML = (total).toFixed(2)+"%";
   } else {
-    document.getElementById("A3Percent").innerHTML = "Percentage Too High!";
+    document.getElementById("A3Percent").innerHTML = "Percentage invalid for other calculations!";
   }
 }
 function A4per() {
@@ -86,6 +201,6 @@ function A4per() {
   if (total < 101) {
     document.getElementById("A4Percent").innerHTML = (total).toFixed(2)+"%";
   } else {
-    document.getElementById("A4Percent").innerHTML = "Percentage Too High!";
+    document.getElementById("A4Percent").innerHTML = "Percentage invalid for other calculations!";
   }
 }
