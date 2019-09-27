a2f2374778d8cd7da4d95815ac0b0468e75b32b7
dhaliwalk <kevindhal@gmail.com>
2019-09-27 14:11:21 -0700

    working

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/calculator'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
   .get('/db', async (req, res) => {
